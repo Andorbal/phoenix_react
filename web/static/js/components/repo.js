907d553a91f646fbcbb45e3c966e6f7f70a25450
Andrew Benz <andorbal@gmail.com>
2016-06-17 06:50:34 -0500

    Add menu link concept

--- web/static/js/components/repo.js
+++ web/static/js/components/repo.js
@@ -2,8 +2,17 @@
 
 import React from 'react'
 import PopupButton from '../controls/popupButton'
+import MenuLink from '../controls/menuLink'
 
 export default React.createClass({
+  handleClone(event) {
+    event.preventDefault();
+    this.menuWindow.closePopup(event);
+  },
+  handleFork(event) {
+    event.preventDefault();
+    this.menuWindow.closePopup(event);
+  },
   render() {
     return (
       <div>
@@ -11,6 +20,16 @@ export default React.createClass({
         <PopupButton text={`Thank ${this.props.params.userName}`}>
           <p>You should thank {this.props.params.userName} for all their hard work!</p>
         </PopupButton>
+        <MenuLink text="Actions" ref={c => this.menuWindow = c}>
+          <ul>
+            <li>
+              <a href="#" onClick={this.handleClone}>Clone</a>
+            </li>
+            <li>
+              <a href="#" onClick={this.handleFork}>Fork</a>
+            </li>
+          </ul>
+        </MenuLink>
       </div>
   )}
 });
